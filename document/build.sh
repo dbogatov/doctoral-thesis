6808bddac3879b47ffcf5c353c3f2f47ff5d7820
Dmytro Bogatov <dmytro@dbogatov.org>
2018-11-29 12:47:33 -0500

    Add release env, test script and task.

--- document/build.sh
+++ document/build.sh
@@ -6,10 +6,29 @@ set -e
 cd "${0%/*}"
 CWD=$(pwd)
 
+COMPILER=pdflatex
 INTERACTION=nonstopmode
 OUTDIR=dist
 JOBNAME=report
 ITERATIONS=3
+PREVIEW=""
+OPEN=false
+
+usage() { echo "Usage: $0 [-o]" 1>&2; exit 1; }
+
+while getopts "o" o; do
+	case "${o}" in
+		o)
+			ITERATIONS=1
+			PREVIEW="\def\preview{true}"
+			OPEN=true
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+shift $((OPTIND-1))
 
 echo "Cleaning up workspace"
 
@@ -20,18 +39,16 @@ for j in `seq 1 $ITERATIONS`;
 do
 	echo "Compiling for the $j time..."
 
-	pdflatex \
-		--interaction=${INTERACTION} \
-		-output-directory=${OUTDIR} \
-		-jobname=${JOBNAME} \
-		main.tex
-
-	if [ "$j" == "1" ]
+	if [ "$j" == "2" ]
 	then
 		biber ${OUTDIR}/${JOBNAME}
 	fi
 
-
+	$COMPILER \
+		--interaction=${INTERACTION} \
+		-output-directory=${OUTDIR} \
+		-jobname=${JOBNAME} \
+		"\def\dummy{} ${PREVIEW} \input main.tex"
 done
 
 echo "Removing build files..."
@@ -39,3 +56,8 @@ echo "Removing build files..."
 rm -f ${OUTDIR}/*.{aux,log,out,xwm,toc,lof,lot,bib,bbl,bcf,blg,xml}
 
 echo "Done."
+
+if [ "$OPEN" == true ]
+then
+	open ${OUTDIR}/${JOBNAME}.pdf
+fi
---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+
+# Ensure that the CWD is set to script's location
+cd "${0%/*}"
+CWD=$(pwd)
+
+#######
+
+echo ">>> Testing CHKTEX..."
+
+chktex -eall -n22 -n46 -n30 -n3 -e16 -I0 -v2 -o chktex.out document/**/*.tex document/*.tex &> /dev/null
+cat chktex.out
+if [ -s chktex.out ];
+then
+	rm -f chktex.*
+	echo ">>> FAIL"
+	exit 1
+else
+	rm -f chktex.*
+fi
+
+echo ">>> Testing CSPELL..."
+
+docker run -it -v $(PWD):/code --entrypoint /bin/bash dbogatov/docker-images:cspell-latest -c "cd /code && cspell -c .vscode/cSpell.json document/**/*.tex document/*.tex"
+
+if [[ $? != 0 ]];
+then
+	echo ">>> FAIL"
+	exit 1
+fi
+
+echo ">>> pass"
+exit 0
