19b49fa649d689cf319686661e5a4013685c8639
Dmytro Bogatov <dmytro@dbogatov.org>
2019-09-17 17:25:49 +0200

    Fix.

--- document/check-fonts.sh
+++ document/check-fonts.sh
@@ -12,7 +12,8 @@ i=0
 while read -r line; do
 	if [ $i -ge 2 ];
 	then
-		res=$(echo $line | cut -d" " -f 5)
+		# fifth from the end
+		res=$(echo $line | rev | cut -d" " -f 5 | rev)
 		[ $res == "yes" ];
 	fi
 	i=$((i+1))
