a5ca429f6056ac45158f9e4053e8a213b86ecba3
Daria Bogatova <daria@dbogatov.org>
2021-01-20 14:00:53 +0100

    Upgrade report template.
    Add draft mode (pdf -> png).
    Add fast mode.
    Update README.
    Remove unnecessary packages and files.
    Add graphics.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Report title
+# LaTeX document
 
 The up-to-date version of the paper is built in CI and resides as artifact.
 
@@ -8,7 +8,14 @@ The up-to-date version of the paper is built in CI and resides as artifact.
 
 ```bash
 bash ./document/build.sh # to compile
+bash ./document/build.sh -f # to compile in fast mode
+bash ./document/build.sh -d # to compile in draft mode
 open ./document/dist/*.pdf # to open
 ```
+
+*Fast mode* does not remove auxiliary files, so the subsequent recompilations are much faster.
+
+*Draft mode* converts all PDF graphics into low-res PNG (output file is `report-draft.pdf`).
+Draft mode requires `imagemagick` to be installed (`brew install` or `apt-get install`).
 
 If using with Overleaf, it suggested to run `git config core.fileMode false`.
--- document/build.sh
+++ document/build.sh
@@ -9,17 +9,21 @@ CWD=$(pwd)
 INTERACTION=nonstopmode
 OUTDIR=dist
 JOBNAME=report
-PREVIEW=""
-OPEN=false
+DRAFT=""
+FAST=false
 SYNCTEX=1
 
-usage() { echo "Usage: $0 [-o]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-f -d]" 1>&2; exit 1; }
 
-while getopts "o" o; do
+while getopts "df" o
+do
 	case "${o}" in
-		o)
-			PREVIEW="\def\preview{true}"
-			OPEN=true
+		f)
+			FAST=true
+			;;
+		d)
+			DRAFT="\def\draft{true}"
+			JOBNAME="$JOBNAME-draft"
 			;;
 		*)
 			usage
@@ -28,27 +32,32 @@ while getopts "o" o; do
 done
 shift $((OPTIND-1))
 
-rm -rf ${OUTDIR}/*
-mkdir -p ${OUTDIR}
+if [ $FAST == false ]
+then
+	mkdir -p ${OUTDIR}/tmp
+	cp ${OUTDIR}/*.pdf ${OUTDIR}/tmp || true
+	rm -f ${OUTDIR}/*.*
+	cp ${OUTDIR}/tmp/*.pdf ${OUTDIR} || true
+	rm -rf ${OUTDIR}/tmp
+	mkdir -p ${OUTDIR}/figures
+fi
 
 if [ -n "$CI_BUILD_REF" ];
 then
 	printf "\providecommand{\\\version}{%s}" $(echo $CI_BUILD_REF | cut -c1-8) > version.tex
 fi
 
-latexmk -cd -dvi- -f -pdf -ps- -time \
-	-jobname=$JOBNAME \
-	-outdir=$OUTDIR \
+latexmk -cd -dvi- -f -pdf -ps- -time -shell-escape \
+	-jobname=${JOBNAME} \
+	-outdir=${OUTDIR} \
 	--synctex=${SYNCTEX} \
 	--interaction=${INTERACTION} \
-	-pdflatex='pdflatex %O "\def\dummy{} '${PREVIEW}' \input %S "' \
+	-pdflatex='pdflatex %O "\def\dummy{} '${REVIEW}' '${DRAFT}' \input %S "' \
 	main
 
-rm -f ${OUTDIR}/*.{aux,log,out,xwm,toc,lof,lot,bib,bbl,bcf,blg,xml,fls,fdb_latexmk}
+if [ $FAST == false ]
+then
+	rm -f ${OUTDIR}/*.{aux,log,out,xwm,toc,lof,lot,bib,bbl,bcf,blg,xml,fls,fdb_latexmk}
+fi
 
 echo "Done."
-
-if [ "$OPEN" == true ]
-then
-	open ${OUTDIR}/${JOBNAME}.pdf
-fi
